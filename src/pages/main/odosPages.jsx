1e68a8a82c8e943845f66bc64caeb4dc24ba8999
fridfn <faridfathonin@gmail.com>
2025-02-24 16:02:50 +0700

    add header title v2

--- src/pages/main/odosPages.jsx
+++ src/pages/main/odosPages.jsx
@@ -18,7 +18,7 @@ const OdosPages = () => {
       <div className='wrapper' id='wrapper-surah'>
        <img style={{zIndex: '20', position: 'absolute', top: '20px'}} src="https://render.gitanimals.org/lines/fridfn?pet-id=671615300271345171" width="600" height="150"/>
        {userData.nama ? (
-         <p>nambahin header</p>
+         <p>nambahin header v2</p>
        ): (null)}
       </div>
      </div>
